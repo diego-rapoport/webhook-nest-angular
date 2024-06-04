eec8e44656c88a19a807ec09cb3ab6d7f90a97ee
Diego Rapoport <diegomrapoport@gmail.com>
2024-06-04 12:56:22 -0300

    base controller of backend

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService
+  ) { }
 
   @Get('/ping')
   ping(): string {
     return this.appService.ping();
+  }
+
+  @Post('/webhook/test')
+  webhookSendtest() {
+    this.httpService.get('http://localhost:4000/api/webhook/ping').subscribe({
+      next: (res) => {
+        return res.data
+      },
+      error: (err) => {
+        return err
+      }
+    })
   }
 
 }
